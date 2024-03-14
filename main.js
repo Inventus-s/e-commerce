2eaf73505fb9a07da010b095b0a4ee5186426a38
Inventus-s <handle.extras@gmail.com>
2024-03-14 21:56:57 +0530

    will work later

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import './style.css'
 import products from "./api/products.json";
 import { showProductContainer } from "./homeProductCards.js";
+import { getCartProductFromLS } from './getCartProducts.js';
+import { updateCartValue } from './updateCartValue.js';
 
 // Define a function named `showProductContainer` that takes an array of products as input.
 showProductContainer(products);
